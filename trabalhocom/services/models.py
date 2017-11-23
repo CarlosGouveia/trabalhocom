18601825a4815024b5d2771ba904f196daaf99a2
CarlosGouveia <carlosadriano_gouveia@hotmail.com>
2017-11-22 22:24:11 -0200

    Alterações responsividade e flavicon - karlos

--- trabalhocom/services/models.py
+++ trabalhocom/services/models.py
@@ -10,6 +10,20 @@ class CategoriaServico(models.Model):
     def __str__(self):
         return self.nome
 
+# class Comment(models.Model):
+#     post = models.ForeignKey('blog.Post', related_name='comments')
+#     author = models.CharField(max_length=200)
+#     text = models.TextField()
+#     created_date = models.DateTimeField(default=timezone.now)
+#     approved_comment = models.BooleanField(default=False)
+
+    # def approve(self):
+    #     self.approved_comment = True
+    #     self.save()
+    #
+    # def __str__(self):
+    #     return self.text
+
 
 class Service(models.Model):
 
